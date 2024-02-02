85a4ef4bfbecc2b86cc4224363cee0ab26d3b3ba
Nitesh Choudhary <100456682+itsmenitesh@users.noreply.github.com>
2024-02-02 12:50:06 +0530

    no use

--- src/Instruction.java
+++ src/Instruction.java
@@ -1,11 +1,1 @@
-public class Instruction {
-    Operation operation;
-    String register;
-    int value;
 
-    public Instruction(Operation operation, String register, int value) {
-        this.operation = operation;
-        this.register = register;
-        this.value = value;
-    }
-}
