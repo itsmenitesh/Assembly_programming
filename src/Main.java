f27db75c4347143b616b70b65c3d0f7b00743310
Nitesh Choudhary <100456682+itsmenitesh@users.noreply.github.com>
2024-02-02 12:47:37 +0530

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+
 public class Main {
     private static Map<String, Integer> registers = new HashMap<>();
 
@@ -23,27 +24,52 @@ public class Main {
     }
 
     public static void executeInstruction(String[] tokens) {
-        Operation operation = Operation.valueOf(tokens[0].toUpperCase());
-        String register = tokens[1];
-        int value = Integer.parseInt(tokens[2].substring(1));
+        if (tokens.length < 2) {
+            System.out.println("Invalid input. Please provide an operation and a register.");
+            return;
+        }
 
-        switch (operation) {
-            case MV:
-                moveValue(register, value);
-                break;
-            case ADD:
-                addValue(register, value);
-                break;
-            case SHOW:
+        Operation operation = null;
+        String register = tokens[1];
+
+        if (tokens.length > 2) {
+            try {
+                int value = Integer.parseInt(tokens[2]);
+                operation = Operation.valueOf(tokens[0].toUpperCase());
+
+                switch (operation) {
+                    case MV:
+                        moveValue(register, value);
+                        break;
+                    case ADD:
+                        addValue(register, value);
+                        break;
+                    case SHOW:
+                        showValue(register);
+                        break;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please provide a valid numeric value for the operation.");
+            } catch (IllegalArgumentException e) {
+                System.out.println("Invalid operation. Please use MV, ADD, SHOW, or EXIT.");
+            }
+        } else {
+            if ("SHOW".equalsIgnoreCase(tokens[0])) {
                 showValue(register);
-                break;
+            } else {
+                System.out.println("Invalid input. Please provide all required parameters.");
+            }
         }
     }
+
+
+
 
     public static void moveValue(String register, int value) {
         registers.put(register, value);
         System.out.println("Moved value " + value + " to register " + register);
     }
+
 
     public static void addValue(String register, int value) {
         int currentValue = registers.getOrDefault(register, 0);
